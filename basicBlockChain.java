b40a13ad05a76405ba876f87c0e34f3c03a7e6c9
Prashant K Pathak <123973637+prashant152005@users.noreply.github.com>
2024-08-09 21:10:17 +0530

    Update basicBlockChain.java

--- basicBlockChain.java
+++ basicBlockChain.java
@@ -27,6 +27,10 @@ public class basicBlockChain {
 		blockchain.add(new Block("Hey im the Fifth block",blockchain.get(blockchain.size()-1).hash));
 		System.out.println("Trying to Mine block 5... ");
 		blockchain.get(4).mineBlock(difficulty);
+
+		blockchain.add(new Block("Hey im the Sixth block",blockchain.get(blockchain.size()-1).hash));
+		System.out.println("Trying to Mine block 6... ");
+		blockchain.get(5).mineBlock(difficulty);
 		
 		System.out.println("\nBlockchain is Valid: " + isChainValid());
 		
